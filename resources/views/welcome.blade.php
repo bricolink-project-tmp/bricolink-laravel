9df60ba4c9a3d3944ce30a29698af27c869c3436
lamjiidii1 <lamjiidii1@gmail.com>
2026-03-05 03:59:52 +0000

    update brand name to bricolink

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,11 +4,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Artisan Booking - Dev</title>
+    <title>BricoLink - Dev</title>
 </head>
 
 <body>
-    <h1>Artisan Booking Platform</h1>
+    <h1>BricoLink Platform</h1>
+    <h2>-------------------------</h2>
     <p>Server is running successfully.</p>
 </body>
 
